2d20a148fd25a422088981d96e487e837e8f124c
Martins Martinsons <martins.martinsons@yahoo.com>
2021-02-01 07:49:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll('.faq-toggle');
+
+buttons.forEach(element => element.addEventListener('click', () => {
+  element.parentNode.classList.toggle('active');
+}))
